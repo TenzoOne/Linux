6775c8ab7fb0ea7fb2805984d31aa654a77db5f1
TenzoOne <carlos.garcia.cruz@hotmail.com>
2021-11-27 22:38:09 -0800

    adding nvim configuration

--- installer.sh
+++ installer.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo installing curl
+
+sudo apt install curl
+
+echo Updating system
+
+sudo apt-get update
+
 echo Installing zsh
 
 sudo apt-get install zsh
@@ -8,7 +16,6 @@ echo installing powerlevel10k
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
 
 echo installing powerline fonts
 
@@ -25,3 +32,14 @@ echo installing Tweak
 sudo apt-get install tweak
 
 echo tweak installated
+
+echo Installing neovim
+
+sudo apt install neovim
+
+echo installing neovim configurations
+ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+              https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo execute nvim and :PlugInstall
+
